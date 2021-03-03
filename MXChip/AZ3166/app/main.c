79c715cc3f6ea7c01750aed7eb1bff8b77c149f1
Cindy Deng <cindydeng@microsoft.com>
2021-03-02 16:15:16 -0800

    hackathon ulib mxchip sample

--- MXChip/AZ3166/app/main.c
+++ MXChip/AZ3166/app/main.c
@@ -16,14 +16,86 @@
 
 #include "azure_config.h"
 
+// ulib imports
+#include "az_ulib_ipc_api.h"
+#include "az_ulib_result.h"
+#include "cipher_v1i1.h"
+#include "cipher_v2i1.h"
+#include "my_consumer.h"
+#include <stdio.h>
+
 #define AZURE_THREAD_STACK_SIZE 4096
 #define AZURE_THREAD_PRIORITY   4
 
 TX_THREAD azure_thread;
 ULONG azure_thread_stack[AZURE_THREAD_STACK_SIZE / sizeof(ULONG)];
 
+static az_ulib_ipc ipc_handle;
+
 void azure_thread_entry(ULONG parameter);
 void tx_application_define(void* first_unused_memory);
+
+int ipc_sample(void)
+{
+    az_result result;
+
+    (void)printf("Start ipc_call_interface sample.\r\n\r\n");
+
+    /*
+    * Create the IPC. It shall be called at the very beginning of the application.
+    * The IPC will prepare itself to receive interfaces.
+    */
+    if ((result = az_ulib_ipc_init(&ipc_handle)) != AZ_OK)
+    {
+    (void)printf("Initialize IPC failed with code %d.\r\n", result);
+    }
+    else
+    {
+    /* Publish cipher v1 with 1 key.
+        * After this point anybody can call the cipher commands through IPC. */
+    cipher_v1i1_create();
+    (void)printf("\r\n");
+
+    /* Consumer will use the math interface. */
+    my_consumer_create();
+    (void)printf("\r\n");
+
+    ///* My consumer try to use math to add numbers. */
+    my_consumer_do_cipher(0);
+    my_consumer_do_cipher(1);
+    (void)printf("\r\n");
+
+    /* Unpublish cipher v1. After this point, any call to cipher will return
+        * AZ_ERROR_ITEM_NOT_FOUND. */
+    cipher_v1i1_destroy();
+    (void)printf("\r\n");
+
+    ///* My consumer try to use math to add numbers. */
+    my_consumer_do_cipher(0);
+    my_consumer_do_cipher(1);
+    (void)printf("\r\n");
+
+    /* Publish cipher v2 with 2 key.
+        * After this point anybody can call the cipher commands through IPC. */
+    cipher_v2i1_create();
+    (void)printf("\r\n");
+
+    ///* My consumer try to use math to add numbers. */
+    my_consumer_do_cipher(0);
+    my_consumer_do_cipher(1);
+    (void)printf("\r\n");
+
+    /* Unpublish cipher v2. After this point, any call to cipher will return
+        * AZ_ERROR_ITEM_NOT_FOUND. */
+    cipher_v2i1_destroy();
+    (void)printf("\r\n");
+
+    /* Consumer will stop to use the math interface. */
+    my_consumer_destroy();
+    (void)printf("\r\n");
+
+    return 1;
+}
 
 void azure_thread_entry(ULONG parameter)
 {
@@ -54,6 +126,13 @@ void azure_thread_entry(ULONG parameter)
         return;
     }
     screen_print("SNTP inited", L0);
+
+    // call ulib sample
+    status = ipc_sample();
+    if(status == 1)
+    {
+        screen_print("IPC example success", L0);
+    }
 
 #ifdef ENABLE_LEGACY_MQTT
     if ((status = azure_iot_mqtt_entry(&nx_ip, &nx_pool[0], &nx_dns_client, sntp_time_get)))
